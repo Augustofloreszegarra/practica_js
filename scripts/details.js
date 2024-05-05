9d6cf5dd78c02794fda718ab579d1cf88a632408
AugustoPC <augusto.floreszegarra@gmail.com>
2024-05-05 15:01:27 -0500

    terminando Obteniendo información en LocalStorage

--- scripts/details.js
+++ scripts/details.js
@@ -2,6 +2,7 @@ const query = window.location.search;
 console.log( query );
 const urlParams = new URLSearchParams( query );
 const id = urlParams.get( 'id' );
+console.log( "primer console log id:"+id );
 const product = products.find( ( product ) => product.id === Number( id ) )
 /* 
 const title = document.getElementsByClassName( 'product-title' );
@@ -92,7 +93,7 @@ function printDetails ( product ) {
             <div class="checkout-process">
             <div class="top">
                 <input id="quantity" type="number" min="1" value="1" onchange="changeSubtotal()"/>
-                <button type="button" class="cart-btn" onclick="saveProduct(event)">
+                <button type="button" class="cart-btn" onclick="saveProduct('${id}')">
                 Añadir al Carrito
                 </button>
             </div>
@@ -118,32 +119,64 @@ function changeSubtotal () {
   document.getElementById( "price" ).innerText = total.toFixed( 2 );
 }
 
-function saveProduct(event) {
-  // Obtener el ID del botón que desencadenó el evento
-  const buttonId = event.target.id;
-  // Extraer el ID del producto del ID del botón asumiendo un formato específico
-  const productId = buttonId.substring(7); // Suponiendo que el ID del botón sigue el formato "addButton-{productId}"
+function saveProduct(id) {
+  /* Se busca en el array products un objeto cuya propiedad id sea 
+  igual al id proporcionado como argumento a la función. 
+  El objeto encontrado se asigna a la variable found. */
+  const found = products.find((product) => product.id == id);
+  
+  /* Se crea un nuevo objeto product con las propiedades id, title, price, image, color y quantity. 
+  Estos valores se obtienen del objeto encontrado en el paso anterior y de los elementos 
+  del DOM seleccionados con los selectores #color y #quantity. */
+  const product = {
+    id: id,
+    title: found.title,
+    price: found.price,
+    image: found.images[0],
+    color: document.querySelector("#color").value,
+    quantity: parseInt(document.querySelector("#quantity").value),
+  };
 
-  // Buscar el producto en el arreglo de productos utilizando el método .find
-  const product = products.find(product => product.id === productId);
+  /* Se obtiene el contenido del carrito desde localStorage. Si el carrito tiene elementos, 
+  se convierte de JSON a un array y se busca si ya existe un producto con el mismo id y color. 
+  Si se encuentra, se incrementa la cantidad del producto existente; de lo contrario, 
+  se agrega el nuevo producto al carrito. Si el carrito está vacío, se agrega el nuevo 
+  producto directamente. */
+  const cart = localStorage.getItem('cart');
+  let cartProducts = [];
 
-  // Si se encuentra el producto
-  if (product) {
-      // Crear un objeto con las propiedades de la compra
-      const purchase = {
-          productId: product.id,
-          productName: product.name,
-          price: product.price,
-          quantity: 1 // Suponiendo que la cantidad por defecto es 1 al añadir al carrito
-      };
+  if (cart) {
+    //se utiliza para convertir la cadena de texto del carrito en un array que se asigna a la variable
+    cartProducts = JSON.parse(cart);
 
-      // Convertir el objeto a formato JSON
-      const purchaseJSON = JSON.stringify(purchase);
-
-      // Guardar el objeto JSON en la memoria del navegador utilizando localStorage
-      localStorage.setItem('cartProduct', purchaseJSON);
+    /* Se utiliza el método findIndex() en el array cartProducts para buscar un producto existente que 
+    coincida con el id y el color del nuevo producto que se desea agregar. La función de flecha 
+    (item) => item.id == id && item.color == product.color se usa como criterio de búsqueda. 
+    Si se encuentra un producto existente que coincida, se guarda su índice en la variable existingProductIndex. */
+    const existingProductIndex = cartProducts.findIndex((item) => item.id == id && item.color == product.color);
+    
+    if (existingProductIndex !== -1) { /* Se verifica si se encontró un producto existente en el carrito que 
+    coincida con el id y el color del nuevo producto. Si existingProductIndex es diferente de -1, 
+    significa que se encontró un producto que coincide. */
+      cartProducts[existingProductIndex].quantity += product.quantity;/* Si se encuentra un producto existente 
+      que coincide, se incrementa la cantidad del producto existente en el carrito sumándole la cantidad 
+      del nuevo producto que se desea agregar. */
+    } else {
+      cartProducts.push(product);
+    }/* En caso de que no se encuentre un producto existente que coincida con el 
+      mismo id y color se agrega el nuevo producto al final del array cartProducts ya que no se encontró un 
+      producto existente con el mismo id y color*/
   } else {
-      console.error('Producto no encontrado');
-  }
+    cartProducts.push(product);
+  }/* Si no hay contenido en el carrito (cuando cart es falso o nulo), en este caso, se agrega el nuevo 
+  producto directamente al array cartProducts ya que no hay productos existentes en el carrito.*/
+
+  /* Se guarda el contenido actualizado del carrito en localStorage después de 
+  convertirlo a formato JSON. */
+  localStorage.setItem('cart', JSON.stringify(cartProducts));
+
+  /* Se imprime en la consola el contenido actual del carrito, 
+  lo que te permite ver los productos con las cantidades actualizadas 
+  después de realizar las operaciones. */
+  console.log('Contenido actual del carrito:', cartProducts);
 }
-changeSubtotal()
