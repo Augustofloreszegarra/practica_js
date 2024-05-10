2e6434bc1ba470d878ba37b454dbebfe591eea1f
AugustoPC <augusto.floreszegarra@gmail.com>
2024-05-09 20:05:14 -0500

    limpiando archivos antiguos

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,44 +1,25 @@
-const cartData = JSON.parse(localStorage.getItem('cart') || '[]');
-console.log(cartData);
+// Oculta el campo de búsqueda en la página
+hideSearch();
 
+// Imprime los íconos en la página
+printIcons();
 
+// Imprime la barra de navegación en la página
+printNavBar(options, "navbar");
 
-function printDetails ( cartData ) {
- 
-  const productDetailSelector = document.getElementById( 'cartSection' )
-  productDetailSelector.innerHTML = `
-    ${ cartData.map( ( cartData ) => `
-    <article class="product-cart" id="cartproducts">
-    <img src="/assets/${ cartData.image }" alt="alt imgaen" class="product-img">
-    <div class="product-details">
-      <span class="product-description">${ cartData.title } ${ cartData.color }</span>
-      <span class="product-description">${ cartData.descriptionLarge }</span>
-      <input type="number" class="product-input" id="quantity" onchange="changeSubtotal()" value="${ cartData.quantity }">
-    </div>
-    <span class="product-price">S/ ${ Number( cartData.price * cartData.quantity ).toFixed( 2 ) }</span>
-    </article>
-    
-    `).join( '' ) }
-    <article class="cart-resume" id="total">
-      <div class="cart-data">
-        <span class="cart-title">Resumen del pedido</span>
-        <div class="cart-total">
-          <span>subtotal</span>
-          <span>S/ 1500.00</span>
-        </div>
-        <span class="cart-tax">Incluye impuestos PAIS y percepión AFIP</span>
-        <button class="cart-btn">Finaliza Pedido</button>
-      </div>
-    </article>
-    `
-    console.log( document.getElementById( "quantity" ).value )
-}
-function changeSubtotal () {
-  const quantityValue = document.getElementById( "quantity" ).value;
+// Imprime el pie de página en la página
+printFooter(options, "footer");
 
-  const price = cartData.price; // Obtener el precio como un número
-  const total = price * parseInt( quantityValue ); // Calcular el total
-  document.getElementById( "price" ).innerText = total.toFixed( 2 );
+// Obtiene los productos del carrito del almacenamiento local y los convierte a un array
+let cartproducts = JSON.parse(localStorage.getItem("cart"));
+
+// Verifica si no hay productos en el carrito y los inicializa como un array vacío si es así
+if (!cartproducts) {
+  cartproducts = [];
 }
 
-printDetails( cartData )
+// Imprime las tarjetas de productos del carrito en la página
+printCartCards(cartproducts, "productscart");
+
+// Calcula y muestra el total del carrito en la página
+printCartTotal(cartproducts, "total");
--- scripts/details.js
+++ scripts/details.js
@@ -1,185 +1,35 @@
-const query = window.location.search;
-console.log( query );
-const urlParams = new URLSearchParams( query );
-const id = urlParams.get( 'id' );
-console.log( "primer console log id:"+id );
-const product = products.find( ( product ) => product.id === Number( id ) )
-/* 
-const title = document.getElementsByClassName( 'product-title' );
-title[ 0 ].innerText = product.title;
+// Oculta el campo de búsqueda en la página
+hideSearch();
 
-const image = document.getElementsByClassName( 'product-image' ); */
+// Imprime los íconos en la página
+printIcons();
 
-/* function pregunta ( product ) {
-  product.id === Number( id )
-}
+// Imprime la barra de navegación en la página
+printNavBar(options, "navbar");
 
-(product) => product.id === Number(id)
-console.log( product ); */
+// Imprime el pie de página en la página
+printFooter(options, "footer");
 
-function printDetails ( product ) {
-  const productDetailSelector = document.getElementById( 'details' )
-  productDetailSelector.innerHTML = `
-        <section class="product-images-block">
-        <div class="product-images">
-        ${ product.images.map( ( image ) => `
-            <img
-            class="mini-img"
-            src="/assets/${ image }"
-            alt="MacBook Pro 13'4"
-            onclick="changeMini(event)"
-            />
-        `).join( '' ) }
-        </div>
-        <img
-            class="big-img"
-            id="big-img"
-            src="/assets/${ product.images[ 0 ] }"
-            alt="MacBook Pro 13'4"
-        />
-        </section>
-        <div class="product-description-block">
-        <h1 class="product-title">${ product.title }</h1>
-        <form class="product-selector">
-            <fieldset class="product-fieldset">
-            <label class="product-label" for="color">Color</label>
-            <select
-                class="product-select"
-                type="text"
-                placeholder="Selecciona un color"
-                id="color"
-            >
-            ${ product.colors.map( ( color ) => `
-                <option value="${ color }">${ color }</option>
-                `) }
-            </select>
-            </fieldset>
-        </form>
-        <div class="product-description">
-            <span class="product-label">Descripción</span>
-            <p>
-            ${ product.descriptionLarge }
-            </p>
-        </div>
-        </div>
-        <div class="product-checkout-block">
-        <div class="checkout-container">
-            <span class="checkout-total-label">Total:</span>
-            <h2 id="price" class="checkout-total-price">${ Number( product.price ).toFixed( 2 ) }</h2>
-            <p class="checkout-description">
-            Incluye impuesto PAIS y percepción AFIP. Podés recuperar USD
-            ${ Number( product.price * product.igv ).toFixed( 2 ) } haciendo la solicitud en Sunat.
-            </p>
-            <ul class="checkout-policy-list">
-            <li>
-                <span class="policy-icon"
-                ><img src="./assets/truck.png" alt="Truck"
-                /></span>
-                <span class="policy-desc"
-                >Agrega el producto al carrito para conocer los costos de
-                envío</span
-                >
-            </li>
-            <li>
-                <span class="policy-icon"
-                ><img src="./assets/plane.png" alt="Plane"
-                /></span>
-                <span class="policy-desc"
-                >Recibí aproximadamente entre 10 y 15 días hábiles,
-                seleccionando envío normal</span
-                >
-            </li>
-            </ul>
-            <div class="checkout-process">
-            <div class="top">
-                <input id="quantity" type="number" min="1" value="1" onchange="changeSubtotal()"/>
-                <button type="button" class="cart-btn" onclick="saveProduct('${id}')">
-                Añadir al Carrito
-                </button>
-            </div>
-            </div>
-        </div>
-        </div>
-    `
-}
+// Obtiene la consulta de la URL actual
+const query = location.search;
 
-function changeMini ( event ) {
-  const selectedSrc = event.target.src;
-  const bigSelector = document.querySelector( "#big-img" );
-  bigSelector.src = selectedSrc;
-}
+// Crea un objeto URLSearchParams a partir de la consulta
+const params = new URLSearchParams(query);
 
-printDetails( product )
+// Obtiene el valor del parámetro "id" de la consulta
+const id = params.get("id");
 
-function changeSubtotal () {
-  const quantityValue = document.getElementById( "quantity" ).value;
+// Imprime los detalles del producto con el id especificado
+printDetails(id);
 
-  const price = product.price; // Obtener el precio como un número
-  const total = price * parseInt( quantityValue ); // Calcular el total
-  document.getElementById( "price" ).innerText = total.toFixed( 2 );
-}
+// Filtra los productos que están en promoción
+const onsale = products.filter((each) => each.onsale);
 
-function saveProduct(id) {
-  /* Se busca en el array products un objeto cuya propiedad id sea 
-  igual al id proporcionado como argumento a la función. 
-  El objeto encontrado se asigna a la variable found. */
-  const found = products.find((product) => product.id == id);
-  
-  /* Se crea un nuevo objeto product con las propiedades id, title, price, image, color y quantity. 
-  Estos valores se obtienen del objeto encontrado en el paso anterior y de los elementos 
-  del DOM seleccionados con los selectores #color y #quantity. */
-  const product = {
-    id: id,
-    title: found.title,
-    price: found.price,
-    image: found.images[0],
-    color: document.querySelector("#color").value,
-    descriptionCort: found.descriptionCort,
-    descriptionLarge: found.descriptionLarge,
-    igv: found.igv,
-    quantity: parseInt(document.querySelector("#quantity").value),
-  };
+// Imprime las tarjetas de productos en promoción en la página
+printProductCards(onsale, "product-container");
 
-  /* Se obtiene el contenido del carrito desde localStorage. Si el carrito tiene elementos, 
-  se convierte de JSON a un array y se busca si ya existe un producto con el mismo id y color. 
-  Si se encuentra, se incrementa la cantidad del producto existente; de lo contrario, 
-  se agrega el nuevo producto al carrito. Si el carrito está vacío, se agrega el nuevo 
-  producto directamente. */
-  const cart = localStorage.getItem('cart');
-  let cartProducts = [];
+// Obtiene los datos del carrito desde el almacenamiento local
+const cartData = JSON.parse(localStorage.getItem('cart') || '[]');
 
-  if (cart) {
-    //se utiliza para convertir la cadena de texto del carrito en un array que se asigna a la variable
-    cartProducts = JSON.parse(cart);
-
-    /* Se utiliza el método findIndex() en el array cartProducts para buscar un producto existente que 
-    coincida con el id y el color del nuevo producto que se desea agregar. La función de flecha 
-    (item) => item.id == id && item.color == product.color se usa como criterio de búsqueda. 
-    Si se encuentra un producto existente que coincida, se guarda su índice en la variable existingProductIndex. */
-    const existingProductIndex = cartProducts.findIndex((item) => item.id == id && item.color == product.color);
-    
-    if (existingProductIndex !== -1) { /* Se verifica si se encontró un producto existente en el carrito que 
-    coincida con el id y el color del nuevo producto. Si existingProductIndex es diferente de -1, 
-    significa que se encontró un producto que coincide. */
-      cartProducts[existingProductIndex].quantity += product.quantity;/* Si se encuentra un producto existente 
-      que coincide, se incrementa la cantidad del producto existente en el carrito sumándole la cantidad 
-      del nuevo producto que se desea agregar. */
-    } else {
-      cartProducts.push(product);
-    }/* En caso de que no se encuentre un producto existente que coincida con el 
-      mismo id y color se agrega el nuevo producto al final del array cartProducts ya que no se encontró un 
-      producto existente con el mismo id y color*/
-  } else {
-    cartProducts.push(product);
-  }/* Si no hay contenido en el carrito (cuando cart es falso o nulo), en este caso, se agrega el nuevo 
-  producto directamente al array cartProducts ya que no hay productos existentes en el carrito.*/
-
-  /* Se guarda el contenido actualizado del carrito en localStorage después de 
-  convertirlo a formato JSON. */
-  localStorage.setItem('cart', JSON.stringify(cartProducts));
-
-  /* Se imprime en la consola el contenido actual del carrito, 
-  lo que te permite ver los productos con las cantidades actualizadas 
-  después de realizar las operaciones. */
-  console.log('Contenido actual del carrito:', cartProducts);
-}
+// Imprime los datos del carrito en la consola del navegador
+console.log(cartData);
